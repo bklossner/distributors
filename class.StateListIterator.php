62f5bb19055974721db80954448e0be19dffb756
Bob <bobklossner@gmail.com>
2017-12-12 23:03:37 -0800

    Updating docs. Adding variable types to function arguments.

--- class.StateListIterator.php
+++ class.StateListIterator.php
@@ -3,25 +3,45 @@
 namespace com\bob\distributors;
 
 /**
- * Created by PhpStorm.
- * User: bob
- * Date: 6/26/17
- * Time: 2:29 PM
+ * Class StateListIterator
+ * @package com\bob\distributors
+ * @version 1.0.0
+ * @author Bob Klossner <farfisa5@gmail.com>
+ * @copyright 2017 Bob Klossner
  */
 class StateListIterator {
-	
+
+	/** @var StateList $stateList */
 	protected $stateList;
+
+	/** @var int $currentState */
 	protected $currentState = 0;
 
+
+	/**
+	 * StateListIterator constructor.
+	 *
+	 * @param StateList $stateList_in
+	 */
 	public function __construct(StateList $stateList_in) {
 		$this->stateList = $stateList_in;
 	}
+
+
+	/**
+	 * @return mixed|null
+	 */
 	public function getCurrentState() {
 		if (($this->currentState > 0) &&
 		    ($this->stateList->getStateCount() >= $this->currentState)) {
 			return $this->stateList->getState($this->currentState);
 		}
 	}
+
+
+	/**
+	 * @return mixed|null
+	 */
 	public function getNextState() {
 		if ($this->hasNextState()) {
 			return $this->stateList->getState(++$this->currentState);
@@ -29,6 +49,11 @@ class StateListIterator {
 			return NULL;
 		}
 	}
+
+
+	/**
+	 * @return bool
+	 */
 	public function hasNextState() {
 		if ($this->stateList->getStateCount() > $this->currentState) {
 			return TRUE;
